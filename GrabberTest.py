5865a44ad887a194c769b735215b70334f2bc073
AmbitEdu <swisehea@macalester.edu>
2022-03-30 18:32:41 -0500

    modify test

--- GrabberTest.py
+++ GrabberTest.py
@@ -6,4 +6,5 @@ from SturdyBotHW3Starter import SturdyBot
 config = {'left-motor':OUTPUT_C, 'right-motor': OUTPUT_B, "medium-motor":OUTPUT_D}
 testBot = SturdyBot('testy', config)
 
-testBot.pointerTurnBy(-120, speed=20)
+testBot.pointerTurnBy(1500, speed=20)
+testBot.pointerTurnBy(-1500, speed=20)
