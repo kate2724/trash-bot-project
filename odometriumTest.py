d5440a387a192247a0278496b008d6cf4dcf5bf9
kate2724 <kbreen1234@gmail.com>
2022-04-13 17:14:17 -0500

    Added Odometry package, moved files to be accessible, created odometriumtest file

---
+++ odometriumTest.py
@@ -0,0 +1,10 @@
+
+from odometry import Odometrium
+from ev3dev2.motor import OUTPUT_A, OUTPUT_B, OUTPUT_C, OUTPUT_D
+from ev3dev2.sensor import INPUT_1, INPUT_2, INPUT_3, INPUT_4
+from SturdyBotHW3Starter import SturdyBot
+import time
+
+config = {'left-motor':OUTPUT_C, 'right-motor': OUTPUT_B, "medium-motor":OUTPUT_D, "color-sensor":INPUT_1}
+mainBot = SturdyBot('mainBot', config)
+
