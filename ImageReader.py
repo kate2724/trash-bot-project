4934f4997d4cf3c3d07e3312e2340c8df21926dd
Katelyn Breen <60229563+kate2724@users.noreply.github.com>
2022-04-17 21:33:24 -0500

    Added seconde camshift to track red colors

--- ImageReader.py
+++ ImageReader.py
@@ -11,19 +11,8 @@ import cv2
 showBackProj = False
 showHistMask = False
 frame = None
-hist = None
-
-def show_hist(hist):
-    """Takes in the histogram, and displays it in the hist window."""
-    bin_count = hist.shape[0]
-    bin_w = 24
-    img = np.zeros((256, bin_count * bin_w, 3), np.uint8)
-    for i in range(bin_count):
-        h = int(hist[i])
-        cv2.rectangle(img, (i * bin_w + 2, 255), ((i + 1) * bin_w - 2, 255 - h), (int(180.0 * i / bin_count), 255, 255),
-                      -1)
-    img = cv2.cvtColor(img, cv2.COLOR_HSV2BGR)
-    cv2.imshow('hist', img)
+histBlue = None
+histRed = None
 
 cam = cv2.VideoCapture(0)
 ret, frame = cam.read()
@@ -37,29 +26,46 @@ if frame is not None:
     # cv2.moveWindow('hist', 700, 100)  # Move to reduce overlap
 
     # Initialize the track window to be the whole frame
-    track_window = (0, 0, wid, hgt)
+    track_windowBlue = (0, 0, wid, hgt)
+    track_windowRed = (0, 0, wid, hgt)
 
     # Initialize the histogram from the stored image
 
     # Here I am faking a stored image with just a couple of blue colors in an array
     # you would want to read the image in from the file instead
-    histImage = np.array([[[110, 70, 50]],
-                          [[111, 128, 128]],
-                          [[115, 100, 100]],
-                          [[117, 64, 50]],
-                          [[117, 200, 200]],
-                          [[118, 76, 100]],
-                          [[120, 101, 210]],
-                          [[121, 85, 70]],
-                          [[125, 129, 199]],
-                          [[128, 81, 78]],
-                          [[130, 183, 111]]], np.uint8)
+    histImageBlue = np.array([[[110, 70, 50]],
+                              [[111, 128, 128]],
+                              [[115, 100, 100]],
+                              [[117, 64, 50]],
+                              [[117, 200, 200]],
+                              [[118, 76, 100]],
+                              [[120, 101, 210]],
+                              [[121, 85, 70]],
+                              [[125, 129, 199]],
+                              [[128, 81, 78]],
+                              [[130, 183, 111]]], np.uint8)
 
-    maskedHistIm = cv2.inRange(histImage, np.array((0., 60., 32.)), np.array((180., 255., 255.)))
-    hist = cv2.calcHist([histImage], [0], maskedHistIm, [16], [0, 180])
-    cv2.normalize(hist, hist, 0, 255, cv2.NORM_MINMAX)
+    histImageRed = np.array([[[50, 70, 110]],
+                              [[128, 128, 200]],
+                              [[100, 100, 130]],
+                              [[50, 64, 117]],
+                              [[200, 200, 250]],
+                              [[100, 76, 118]],
+                              [[210, 101, 120]],
+                              [[70, 85, 121]],
+                              [[199, 129, 210]],
+                              [[78, 81, 128]],
+                              [[111, 120, 200]]], np.uint8)
 
-    hist = hist.reshape(-1)
+    maskedHistImBlue = cv2.inRange(histImageBlue, np.array((0., 60., 32.)), np.array((180., 255., 255.)))
+    histBlue = cv2.calcHist([histImageBlue], [0], maskedHistImBlue, [16], [0, 180])
+    cv2.normalize(histBlue, histBlue, 0, 255, cv2.NORM_MINMAX)
+    histBlue = histBlue.reshape(-1)
+
+    maskedHistImRed = cv2.inRange(histImageRed, np.array((0., 60., 32.)), np.array((180., 255., 255.)))
+    histRed = cv2.calcHist([histImageRed], [0], maskedHistImRed, [16], [0, 180])
+    cv2.normalize(histRed, histRed, 0, 255, cv2.NORM_MINMAX)
+    histRed = histRed.reshape(-1)
     # show_hist(hist)
 
     # start processing frames
@@ -76,19 +82,27 @@ if frame is not None:
         if showHistMask:
             vis[mask == 0] = 0
 
-        prob = cv2.calcBackProject([hsv], [0], hist, [0, 180], 1)
-        prob &= mask
+        probBlue = cv2.calcBackProject([hsv], [0], histBlue, [0, 180], 1)
+        probBlue &= mask
+        term_critBlue = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 1)
+        track_boxBlue, track_windowBlue = cv2.CamShift(probBlue, track_windowBlue, term_critBlue)
+
+        probRed = cv2.calcBackProject([hsv], [0], histRed, [0, 180], 1)
+        probRed &= mask
         term_crit = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 1)
-        track_box, track_window = cv2.CamShift(prob, track_window, term_crit)
+        track_boxRed, track_windowRed = cv2.CamShift(probRed, track_windowRed, term_crit)
 
         if showBackProj:
-            vis[:] = prob[..., np.newaxis]
+            vis[:] = probBlue[..., np.newaxis]
+            vis[:] = probRed[..., np.newaxis]
         try:
-            cv2.ellipse(vis, track_box, (0, 0, 255), 2)
+            cv2.ellipse(vis, track_boxBlue, (255, 0, 0), 2)
+            cv2.ellipse(vis, track_boxRed, (0, 0, 255), 2)
         except:
-            print("Track box:", track_box)
+            print("Track box:", track_boxBlue)
+            print("Track box:", track_boxRed)
 
-        cv2.imshow('camshift', vis)
+        cv2.imshow('find blue', vis)
 
         ch = chr(0xFF & cv2.waitKey(5))
         if ch == 'q':
