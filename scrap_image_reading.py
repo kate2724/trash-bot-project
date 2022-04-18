aca17c55ee2c9dddf33595ffcf78b141b876d8b5
Katelyn Breen <60229563+kate2724@users.noreply.github.com>
2022-04-18 17:57:58 -0500

    added code to allow program to detect red and blue at the same time. Labels blue objects with green contour, and red objects with blue contour

--- scrap_image_reading.py
+++ scrap_image_reading.py
@@ -9,20 +9,20 @@ if frame is not None:
     while True:
         ret, frame = cam.read()
         hsv = cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
-        cv2.imshow("hsv", hsv)
+        # cv2.imshow("hsv", hsv)
 
         blueFiltered = cv2.inRange(hsv, (100, 100, 40), (150, 240, 230))
         redFiltered = cv2.inRange(hsv, (150, 100, 115), (190, 210, 170))
         # cv2.imshow("final", blueFiltered)
-        cv2.imshow("final", redFiltered)
+        # cv2.imshow("final", redFiltered)
 
-        # contrs, hier = cv2.findContours(blueFiltered, cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-        contrs, hier = cv2.findContours(redFiltered, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        contrsBlue, hierBlue = cv2.findContours(blueFiltered, cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+        contrsRed, hierRed = cv2.findContours(redFiltered, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
         # cv2.drawContours(frame, contrs, -1, (0, 255, 0), 3)
 
-        if len(contrs) > 0:
-            c = max(contrs, key=cv2.contourArea)
-            x, y, w, h = cv2.boundingRect(c)
+        if len(contrsBlue) > 0:
+            cBlue = max(contrsBlue, key=cv2.contourArea)
+            xBlue, yBlue, wBlue, hBlue = cv2.boundingRect(cBlue)
             # actual coordinates are (x,y),(x+w,y), (x,y+h), (x+w,y+h)
             # robot turns until coordinates are centered
             # robot forward until area of coordinates passes certain threshold (means the robot is close enough)
@@ -31,9 +31,21 @@ if frame is not None:
             # same process as before, until it reaches dumpster
 
             # draw the biggest contour (c) in green
-            if cv2.contourArea(c) > 1250:
-                rectangle = cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-                print("coordinates: ", x, y, w, h)
+            if cv2.contourArea(cBlue) > 1250:
+                rectangle = cv2.rectangle(frame, (xBlue, yBlue), (xBlue + wBlue, yBlue + hBlue), (0, 255, 0), 2)
+                print("blue coordinates: ", xBlue, yBlue, wBlue, hBlue)
+                objectFound = True
+            else:
+                objectFound = False
+
+        if len(contrsRed) > 0:
+            cRed = max(contrsRed, key=cv2.contourArea)
+            xRed, yRed, wRed, hRed = cv2.boundingRect(cRed)
+
+            # draw the biggest contour (c) in blue
+            if cv2.contourArea(cRed) > 1250:
+                rectangle = cv2.rectangle(frame, (xRed, yRed), (xRed + wRed, yRed + hRed), (255, 0, 0), 2)
+                print("red coordinates: ", xRed, yRed, wRed, hRed)
                 objectFound = True
             else:
                 objectFound = False
