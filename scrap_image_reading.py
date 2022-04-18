41b304fdb5191d98fa2c4a03899b37f4ed86ceb1
Katelyn Breen <60229563+kate2724@users.noreply.github.com>
2022-04-18 17:51:08 -0500

    changed threshold for redFiltered to more accurately detect red colors

--- scrap_image_reading.py
+++ scrap_image_reading.py
@@ -8,23 +8,21 @@ objectFound = False
 if frame is not None:
     while True:
         ret, frame = cam.read()
-        hsv=cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
-        # cv2.imshow("hue", img2)
+        hsv = cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
+        cv2.imshow("hsv", hsv)
 
-        # img3= cv2.inRange(img2, (100,50,40), (255,100,100))
         blueFiltered = cv2.inRange(hsv, (100, 100, 40), (150, 240, 230))
+        redFiltered = cv2.inRange(hsv, (150, 100, 115), (190, 210, 170))
+        # cv2.imshow("final", blueFiltered)
+        cv2.imshow("final", redFiltered)
 
-        redFiltered = cv2.inRange(hsv, (50, 40, 150), (120, 150, 255))
-        # cv2.imshow("final", redFiltered)
-
-        contrs, hier = cv2.findContours(blueFiltered, cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-        # contrs, hier = cv2.findContours(redFiltered, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        # contrs, hier = cv2.findContours(blueFiltered, cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+        contrs, hier = cv2.findContours(redFiltered, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
         # cv2.drawContours(frame, contrs, -1, (0, 255, 0), 3)
 
         if len(contrs) > 0:
             c = max(contrs, key=cv2.contourArea)
             x, y, w, h = cv2.boundingRect(c)
-            print("coordinates: ", x,y,w,h)
             # actual coordinates are (x,y),(x+w,y), (x,y+h), (x+w,y+h)
             # robot turns until coordinates are centered
             # robot forward until area of coordinates passes certain threshold (means the robot is close enough)
@@ -33,11 +31,12 @@ if frame is not None:
             # same process as before, until it reaches dumpster
 
             # draw the biggest contour (c) in green
-            if(cv2.contourArea(c)>1250):
+            if cv2.contourArea(c) > 1250:
                 rectangle = cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-                objectFound=True
+                print("coordinates: ", x, y, w, h)
+                objectFound = True
             else:
-                objectFound=False
+                objectFound = False
         cv2.imshow('Contours', frame)
 
         # print("object Found: ", objectFound)
