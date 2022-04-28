e954868133e639677b2f03bb71728e436c475ce5
AmbitEdu <swisehea@macalester.edu>
2022-04-27 21:08:11 -0500

    begin implementing pseudocode

--- RobotMain.py
+++ RobotMain.py
@@ -3,31 +3,90 @@ from ev3dev2.sensor import INPUT_1, INPUT_2, INPUT_3, INPUT_4
 from SturdyBotHW3Starter import SturdyBot
 import time
 
-config = {'left-motor':OUTPUT_C, 'right-motor': OUTPUT_B, "medium-motor":OUTPUT_D, "color-sensor":INPUT_1,"gyro-sensor":INPUT_2}
-mainBot = SturdyBot('mainBot', config)
+class TrashBot:
+    def __init__(self):
+        config = {'left-motor':OUTPUT_C, 'right-motor': OUTPUT_B, "medium-motor":OUTPUT_D, "color-sensor":INPUT_1,"gyro-sensor":INPUT_2}
+        self.mainBot = SturdyBot('mainBot', config)
+        self.initialize()
+        self.state, self.foundTrash, self.remainingTrash, self.sensorResult, self.halt = (None for i in range(5))
 
-def setup(time_limit):
-    startTime = time.time()
-    elapsedTime = 0.0
-
-    while elapsedTime < time_limit:
-        step()
-        elapsedTime = time.time() - startTime
-        light = mainBot.readReflect()
-        print("light", light)
-        if light >= 10:
-            print("Success! elapsed time:", elapsedTime)
-            mainBot.stop()
-            mainBot.snd.speak("found color")
-            break
-    if elapsedTime >= time_limit:
-        mainBot.stop()
-        print("Failure. ran out of time (limit", time_limit, "seconds")
-        mainBot.snd.speak("I failed :(")
-    mainBot.stop()
+    def initialize(self):
+        self.state = "looking for trash"
+        self.foundTrash = []
+        self.remainingTrash = ['blue']
+        self.sensorResult = None
+        self.halt = False  # emergency stop
 
 
-def step():
-    mainBot.forward(10)
+    def run(s, time_limit):
+        s.initialize()
+        startTime = time.time()
+        elapsedTime = 0.0
+        while elapsedTime < time_limit and len(s.remainingTrash) > 0 and not s.halt:
+            s.sensorResult = SensorReading(s.mainBot)
+            s.cleanUpStep()
+            elapsedTime = time.time() - startTime
+            if s.mainBot.bttn.backspace:
+                s.halt = True
+        if len(s.remainingTrash) == 0:
+            print("success")
+        else:
+            print("timeout")
+        s.mainBot.stop()
 
-setup(20)
+    def cleanUpStep(s):
+        if s.state == "looking for trash":
+            s.searchForTrash()
+        elif s.state == "grabbing trash":
+            s.grabTrash()
+        elif s.state == "transporting trash":
+            s.transportTrash()
+        elif s.state == "releasing trash":
+            s.releaseTrash()
+
+    def searchForTrash(s):
+        if s.sensorResult.foundTrash:
+
+
+
+
+class SensorReading:
+    def __init__(self, robot):
+        # creates a new sensor reading from the robot's pixy cam.
+        self.trashList = self.readForTrash(robot)
+        self.foundTrash = len(self.trashList) > 0
+        self.dumpsterX = self.readForDumpster(robot)
+        self.foundDumpster = self.dumpsterX >= 0
+
+    def readForTrash(self, robot):
+        # do stuff with pixy cam
+        return ()
+
+    def readForDumpster(self, robot):
+        # do stuff with pixy cam
+        return -1
+
+    # def setup(time_limit):
+    #     startTime = time.time()
+    #     elapsedTime = 0.0
+    #
+    #     while elapsedTime < time_limit:
+    #         step()
+    #         elapsedTime = time.time() - startTime
+    #         light = mainBot.readReflect()
+    #         print("light", light)
+    #         if light >= 10:
+    #             print("Success! elapsed time:", elapsedTime)
+    #             mainBot.stop()
+    #             mainBot.snd.speak("found color")
+    #             break
+    #     if elapsedTime >= time_limit:
+    #         mainBot.stop()
+    #         print("Failure. ran out of time (limit", time_limit, "seconds")
+    #         mainBot.snd.speak("I failed :(")
+    #     mainBot.stop()
+    #
+    # def step():
+    #     mainBot.forward(10)
+    #
+    # setup(20)
